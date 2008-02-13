31c36e14e4216acb0185dd65ac16ddac8d5b3a6f
Admin <admin@chrisoldwood.com>
2008-02-13 13:53:18 +0000

    Added NumBytes<> template function for calculating byte counts from chars. Added many aliases for the string functions to support dual ANSI/Unicode builds.

--- tstring.hpp
+++ tstring.hpp
@@ -21,14 +21,48 @@
 #pragma pop_macro("new")
 #include <string>
 
+namespace Core
+{
+
+////////////////////////////////////////////////////////////////////////////////
+//! Calculate the number of bytes required to represent a string of the given
+//! number of characters.
+
+template<typename CharT>
+inline size_t NumBytes(size_t nChars)
+{
+	return (nChars * sizeof(CharT));
+}
+
+////////////////////////////////////////////////////////////////////////////////
+//! Calculate the number of bytes required to represent the string.
+
+inline size_t NumBytes(const char* psz)
+{
+	return NumBytes<char>(strlen(psz));
+}
+
+////////////////////////////////////////////////////////////////////////////////
+//! Calculate the number of bytes required to represent the string.
+
+inline size_t NumBytes(const wchar_t* psz)
+{
+	return NumBytes<wchar_t>(wcslen(psz));
+}
+
+//namespace Core
+}
+
 ////////////////////////////////////////////////////////////////////////////////
 // The following #defines mirror the Windows style TCHAR definitions. These
 // allow the same codebase to support both ANSI and UNICODE builds. They use a
 // consistent 't' prefix for both the types and functions rather than the _tcs
 // prefix used by the Windows header.
 
+//
 // ANSI build.
-#ifndef _UNICODE
+//
+#ifdef ANSI_BUILD
 
 // Basic types.
 typedef char			tchar;		//!< Build agnostic character type. Maps to char or wchat_t.
@@ -36,18 +70,38 @@ typedef char			tchar;		//!< Build agnostic character type. Maps to char or wchat
 
 namespace std
 {
-typedef	std::string		tstring;	//!< Build agnostic string type. Maps to string or wstring.
+typedef	std::string		tstring;	//!< Build agnostic string type.
+typedef std::ostream	tostream;	//!< Build agnostic output stream type.
+typedef std::istream	tistream;	//!< Build agnostic input stream type.
 }
 
 // String functions.
 #define tstrlen			strlen
+#define tstrlwr			_strlwr
+#define tstrupr			_strupr
+#define tstrcmp			strcmp
+#define tstricmp		_stricmp
+#define tstrncmp		strncmp
+#define tstrnicmp		_strnicmp
+#define tstrcpy			strcpy
+#define tstrncpy		strncpy
+#define tstrcat			strcat
+#define tstrtok			strtok
+#define tstrchr			strchr
+#define	tstrrchr		strrchr
+#define	tstrstr			strstr
+#define tstrtol			strtol
+#define tstrtoul		strtoul
+#define tstrtod			strtod
 
 // Standard streams
 #define tcin			cin
 #define tcout			cout
 #define tcerr			cerr
 
+//
 // UNICODE build.
+//
 #else
 
 // Basic types.
@@ -56,11 +110,29 @@ typedef wchar_t			tchar;		//!< Build agnostic character type. Maps to char or wc
 
 namespace std
 {
-typedef	std::wstring	tstring;	//!< Build agnostic string type. Maps to string or wstring.
+typedef	std::wstring	tstring;	//!< Build agnostic string type.
+typedef std::wostream	tostream;	//!< Build agnostic output stream type.
+typedef std::wistream	tistream;	//!< Build agnostic input stream type.
 }
 
 // String functions.
 #define tstrlen			wcslen
+#define tstrlwr			_wcslwr
+#define tstrupr			_wcsupr
+#define tstrcmp			wcscmp
+#define tstricmp		_wcsicmp
+#define tstrncmp		wcsncmp
+#define tstrnicmp		_wcsnicmp
+#define tstrcpy			wcscpy
+#define tstrncpy		wcsncpy
+#define tstrcat			wcscat
+#define tstrtok			wcstok
+#define tstrchr			wcschr
+#define	tstrrchr		wcsrchr
+#define	tstrstr			wcsstr
+#define tstrtol			wcstol
+#define tstrtoul		wcstoul
+#define tstrtod			wcstod
 
 // Standard streams
 #define tcin			wcin
