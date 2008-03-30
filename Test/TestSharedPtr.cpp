240da86ddc891281a8254c9d2821925af8da0e94
Admin <admin@chrisoldwood.com>
2008-03-30 21:11:55 +0000

    Moved IFacePtr tests to WCL.

--- Test/TestSharedPtr.cpp
+++ Test/TestSharedPtr.cpp
@@ -1,64 +1,27 @@
 ////////////////////////////////////////////////////////////////////////////////
-//! \file   TestPointers.cpp
-//! \brief  The unit tests for the smart pointer types.
+//! \file   TestSharedPtr.cpp
+//! \brief  The unit tests for the SharedPtr class.
 //! \author Chris Oldwood
 
 #include "stdafx.h"
 #include <Core/UnitTest.hpp>
-#include <Core/IFacePtr.hpp>
+#include <Core/SharedPtr.hpp>
+#include "PtrTest.hpp"
 
 ////////////////////////////////////////////////////////////////////////////////
-//! The test interface.
+//! The unit tests for the SharedPtr class.
 
-class IPtrTest
-{
-public:
-	virtual ulong AddRef() = 0;
-	virtual ulong Release() = 0;
-	virtual void  Run() = 0;
-};
-
-////////////////////////////////////////////////////////////////////////////////
-//! The test concrete class.
-
-class PtrTest : public IPtrTest
-{
-public:
-	PtrTest()
-		: m_nRefCount(1)
-	{ }
-
-	virtual ulong AddRef()
-	{
-		return ++m_nRefCount;
-	}
-
-	virtual ulong Release()
-	{
-		if (--m_nRefCount == 0)
-			delete this;
-		return m_nRefCount;
-	}
-
-	virtual void Run()
-	{ }
-
-	ulong m_nRefCount;
-};
-
-////////////////////////////////////////////////////////////////////////////////
-//! The unit tests for the smart pointer types.
-
-void TestPointers()
-{
+void TestSharedPtr()
 {
 	typedef Core::SharedPtr<PtrTest> TestPtr;
+	typedef Core::SharedPtr<Derived> DerivedPtr;
+	typedef Core::SharedPtr<Unrelated> UnrelatedPtr;
 
 	TestPtr pTest1;
-//	TestPtr pTest2 = new Test;			// Shouldn't compile.
+//	TestPtr pTest2 = new PtrTest;			// Shouldn't compile.
 	TestPtr pTest3 = TestPtr(new PtrTest);
 	TestPtr pTest4(new PtrTest);
-	TestPtr pTest5(TestPtr(new PtrTest));
+	TestPtr pTest5(pTest3);
 
 	TEST_TRUE(pTest1.Get() == nullptr);
 	TEST_TRUE(pTest3.Get() != nullptr);
@@ -67,7 +30,7 @@ void TestPointers()
 
 	pTest1 = pTest3;
 	pTest1 = pTest1;
-//	Test* pRaw1 = pTest1;				// Shouldn't compile.
+//	PtrTest* pRaw1 = pTest1;				// Shouldn't compile.
 
 	TEST_TRUE(pTest1.Get() == pTest3.Get());
 
@@ -96,42 +59,37 @@ void TestPointers()
 	pTest1.Reset();
 
 	TEST_TRUE(pTest1.Get() == nullptr);
-}
-{
-	typedef Core::IFacePtr<IPtrTest> ITestPtr;
+	TEST_THROWS(*pTest1);
+	TEST_THROWS(pTest1->Release());
 
-	ITestPtr pITest1;
-//	ITestPtr pITest2 = new Test;			// Shouldn't compile.
-	ITestPtr pITest3 = ITestPtr(new PtrTest);
-	ITestPtr pITest4(new PtrTest);
-	ITestPtr pITest5(ITestPtr(new PtrTest));
+//	delete pTest1;	// Shouldn't compile.
 
-	TEST_TRUE(pITest1.Get() == nullptr);
-	TEST_TRUE(pITest3.Get() != nullptr);
-	TEST_TRUE(pITest4.Get() != nullptr);
-	TEST_TRUE(pITest5.Get() != nullptr);
+	TestPtr pBase1(new PtrTest);
+	TestPtr pBase2(new PtrTest);
+	TestPtr pBase3(new Derived);
 
-	pITest1 = pITest3;
-	pITest1 = pITest1;
-//	ITest* pIRaw = pITest1;				// Shouldn't compile.
+	DerivedPtr pDerived1(new Derived);
+	DerivedPtr pDerived2(new Derived);
+	TestPtr    pBase4(pDerived1);
 
-	TEST_TRUE(pITest1.Get() == pITest3.Get());
+	UnrelatedPtr pUnrelated;
 
-	pITest1->Run();
-	(*pITest1).Run();
+	pBase2 = pDerived1;
+	pBase2 = pDerived1;
 
-//	TEST_FALSE((pITest1 == NULL) || (pITest1 != NULL));	// Shouldn't compile.
-	TEST_FALSE(!pITest1);
+	TEST_TRUE(pBase2.Get() == pDerived1.Get());
 
-	TEST_TRUE(pITest1 == pITest3);
-	TEST_TRUE(pITest1 != pITest4);
+	pDerived2 = Core::static_ptr_cast<Derived>(pBase3);
 
-	pITest1.Release();
+	TEST_TRUE(pDerived2.Get() == pBase3.Get());
 
-	TEST_TRUE(pITest1.Get() == nullptr);
+	pDerived2 = Core::dynamic_ptr_cast<Derived>(pBase3);
 
-	delete pITest4.Detach();
+	TEST_TRUE(pDerived2.Get() == pBase3.Get());
 
-	TEST_TRUE(pITest4.Get() == nullptr);
-}
+//	pUnrelated = Core::static_ptr_cast<Unrelated>(pBase1);	// Shouldn't compile.
+
+	pUnrelated = Core::dynamic_ptr_cast<Unrelated>(pBase1);	// Shouldn't compile.
+
+	TEST_TRUE(pUnrelated.Get() == nullptr);
 }
