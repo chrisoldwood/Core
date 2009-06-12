f6afd31e5ccfb450b70774cbb06eb6fd4a5039ef
Admin <admin@chrisoldwood.com>
2009-06-12 17:42:43 +0000

    Initial version.

---
+++ Test/TestInterlocked.cpp
@@ -0,0 +1,19 @@
+////////////////////////////////////////////////////////////////////////////////
+//! \file   TestInterlocked.cpp
+//! \brief  The unit tests for the Interlocked functions.
+//! \author Chris Oldwood
+
+#include "stdafx.h"
+#include <Core/UnitTest.hpp>
+#include <Core/Interlocked.hpp>
+
+////////////////////////////////////////////////////////////////////////////////
+//! The unit tests for the Interlocked functions.
+
+void TestInterlocked()
+{
+	long value = 1;
+
+	TEST_TRUE(Core::atomicIncrement(value) == 2);
+	TEST_TRUE(Core::atomicDecrement(value) == 1);
+}
