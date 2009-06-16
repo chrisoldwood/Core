e9c0963c6bb251c61f036fc09f5c0c78b744ae94
Admin <admin@chrisoldwood.com>
2009-06-16 10:53:08 +0000

    Wrapped the _Interlocked*() functions so that we can use the intrinsic versions under VC++ and the Windows SDK ones with GCC etc.

--- Interlocked.hpp
+++ Interlocked.hpp
@@ -11,14 +11,73 @@
 #pragma once
 #endif
 
+// Visual C++
+#ifdef _MSC_VER
+
 ////////////////////////////////////////////////////////////////////////////////
-// Manually define the intrinsic forms of the Interlocked*() functions to avoid
+// Manually define the intrinsic forms of the _Interlocked*() functions to avoid
 // bringing in <windows.h>.
 
-extern "C" long __cdecl _InterlockedIncrement(volatile long *lpValue);
-extern "C" long __cdecl _InterlockedDecrement(volatile long *lpValue);
+extern "C" long __cdecl _InterlockedIncrement(volatile long* lpValue);
+extern "C" long __cdecl _InterlockedDecrement(volatile long* lpValue);
 
 #pragma intrinsic(_InterlockedIncrement)
 #pragma intrinsic(_InterlockedDecrement)
+
+namespace Core
+{
+
+////////////////////////////////////////////////////////////////////////////////
+//! Thread-safe function for incrementing the value by one.
+
+inline long atomicIncrement(volatile long& value)
+{
+	return _InterlockedIncrement(&value);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+//! Thread-safe function for decrementing the value by one.
+
+inline long atomicDecrement(volatile long& value)
+{
+	return _InterlockedDecrement(&value);
+}
+
+//namespace Core
+}
+
+// Not Visual C++
+#else
+
+////////////////////////////////////////////////////////////////////////////////
+// Manually define the exported Windows SDK Interlocked*() functions to avoid
+// bringing in <windows.h>.
+
+extern "C" long __stdcall InterlockedIncrement(volatile long* lpValue);
+extern "C" long __stdcall InterlockedDecrement(volatile long* lpValue);
+
+namespace Core
+{
+
+////////////////////////////////////////////////////////////////////////////////
+//! Thread-safe function for incrementing the value by one.
+
+inline long atomicIncrement(volatile long& value)
+{
+	return InterlockedIncrement(&value);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+//! Thread-safe function for decrementing the value by one.
+
+inline long atomicDecrement(volatile long& value)
+{
+	return InterlockedDecrement(&value);
+}
+
+//namespace Core
+}
+
+#endif
 
 #endif // CORE_INTERLOCKED_HPP
