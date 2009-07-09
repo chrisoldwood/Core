38bbf82c453c03fb8382954baed305b2a3ed5ab3
Admin <admin@chrisoldwood.com>
2009-07-09 20:55:12 +0000

    Fixed code to compile under GCC 4.4.0. Reimplemented the parse() and format() methods to use a common template based implementation. Implemented the 64-bit parse() and format() functions.

--- StringUtils.cpp
+++ StringUtils.cpp
@@ -24,29 +24,51 @@ namespace Core
 
 tstring FmtEx(const tchar* pszFormat, va_list args)
 {
-	// Allocate the buffer.
 #ifdef _MSC_VER
-	int nLength = _vsctprintf(pszFormat, args);
-#else
-	int nLength = 1024;
-#endif
+
+	// Allocate the buffer.
+	size_t nLength = _vsctprintf(pszFormat, args);
 
 	tstring str(nLength, TXT('\0'));
 
 	// Format the string.
 	int nResult = _vsntprintf(&str[0], nLength, pszFormat, args);
 
-	ASSERT(nResult == nLength);
+	ASSERT(static_cast<size_t>(nResult) == nLength);
 
-	// Check for buffer overrun.
+	// Handle any errors.
 	if (nResult < 0)
-		throw BadLogicException(Fmt(TXT("Insufficient buffer size calculated in Fmt(). Result: %d"), nResult));
-
-#ifndef _MSC_VER
-	str.resize(nResult);
-#endif
+		throw BadLogicException(Fmt(TXT("Invalid format or buffer size used in Fmt(). Result: %d"), nResult));
 
 	return str;
+
+#else
+
+	// Allocate an initial buffer.
+	size_t nLength = 256;
+
+	tstring str(nLength, TXT('\0'));
+
+	int nResult = -1;
+
+	// Format the string, growing the buffer and repeating if necessary.
+	while ((nResult = _vsntprintf(&str[0], nLength, pszFormat, args)) == -1)
+	{
+		nLength *= 2;
+		str.resize(nLength, TXT('\0'));
+	}
+
+	ASSERT(static_cast<size_t>(nResult) <= nLength);
+
+	// Handle any errors.
+	if (nResult < 0)
+		throw BadLogicException(Fmt(TXT("Invalid format or buffer size used in Fmt(). Result: %d"), nResult));
+
+	str.resize(nResult);
+
+	return str;
+
+#endif
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -59,6 +81,85 @@ tstring Fmt(const tchar* pszFormat, ...)
 	va_start(args, pszFormat);
 
 	return FmtEx(pszFormat, args);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Traits to invoke the underlying formatting and parsing functions.
+
+template<typename T>
+struct FormatTraits
+{};
+
+template<>
+struct FormatTraits<int>
+{
+	static int format(tchar* buffer, size_t size, int value)
+	{
+		return _sntprintf(buffer, size, TXT("%d"), value);
+	}
+
+	static long parse(const tchar* nptr, tchar** endptr, int base)
+	{
+		return tstrtol(nptr, endptr, base);
+	}
+};
+
+template<>
+struct FormatTraits<uint>
+{
+	static int format(tchar* buffer, size_t size, uint value)
+	{
+		return _sntprintf(buffer, size, TXT("%u"), value);
+	}
+
+	static ulong parse(const tchar* nptr, tchar** endptr, int base)
+	{
+		return tstrtoul(nptr, endptr, base);
+	}
+};
+
+template<>
+struct FormatTraits<longlong>
+{
+	static int format(tchar* buffer, size_t size, longlong value)
+	{
+		return _sntprintf(buffer, size, TXT("%I64d"), value);
+	}
+
+	static longlong parse(const tchar* nptr, tchar** endptr, int base)
+	{
+		return tstrtoll(nptr, endptr, base);
+	}
+};
+
+template<>
+struct FormatTraits<ulonglong>
+{
+	static int format(tchar* buffer, size_t size, ulonglong value)
+	{
+		return _sntprintf(buffer, size, TXT("%I64u"), value);
+	}
+
+	static ulonglong parse(const tchar* nptr, tchar** endptr, int base)
+	{
+		return tstrtoull(nptr, endptr, base);
+	}
+};
+
+////////////////////////////////////////////////////////////////////////////////
+//! Generic function for parsing integers.
+
+template<typename T, typename Traits>
+tstring formatInteger(const T& value)
+{
+	tchar buffer[std::numeric_limits<T>::digits10+3]; // '+-' + (digits+1) + '\0'
+
+	int result = Traits::format(buffer, ARRAY_SIZE(buffer), value);
+
+	if (result < 0)
+		throw BadLogicException(Core::Fmt(TXT("Insufficient sized buffer passed in format<>(). Result: %d"), result));
+
+	return buffer;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -76,14 +177,7 @@ tstring format(const bool& value)
 template<>
 tstring format(const int& value)
 {
-	tchar buffer[std::numeric_limits<int>::digits10+3]; // '+-' + (digits+1) + '\0'
-
-	int result = _sntprintf(buffer, ARRAY_SIZE(buffer), TXT("%d"), value);
-
-	if (result < 0)
-		throw BadLogicException(Core::Fmt(TXT("Insufficient sized buffer passed in format<int>(). Result: %d"), result));
-
-	return buffer;
+	return formatInteger< int, FormatTraits<int> >(value);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -92,25 +186,61 @@ tstring format(const int& value)
 template<>
 tstring format(const uint& value)
 {
-	tchar buffer[std::numeric_limits<uint>::digits10+2]; // (digits+1) + '\0'
-
-	int result = _sntprintf(buffer, ARRAY_SIZE(buffer), TXT("%u"), value);
-
-	if (result < 0)
-		throw BadLogicException(Core::Fmt(TXT("Insufficient sized buffer passed in format<uint>(). Result: %d"), result));
-
-	return buffer;
+	return formatInteger< uint, FormatTraits<uint> >(value);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-// Format a 64-bit unsigned integer value into a string.
+//! Format a signed long long integer value into a string.
 
 template<>
-tstring format(const uint64& /*value*/)
+tstring format(const longlong& value)
 {
-	ASSERT_FALSE();
+	return formatInteger< longlong, FormatTraits<longlong> >(value);
+}
 
-	return _T("");
+////////////////////////////////////////////////////////////////////////////////
+//! Format a unsigned long long integer value into a string.
+
+template<>
+tstring format(const ulonglong& value)
+{
+	return formatInteger< ulonglong, FormatTraits<ulonglong> >(value);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+//! Generic function for parsing integers.
+
+template<typename T, typename Traits>
+T parseInteger(const tstring& buffer)
+{
+	// Get buffer iterators.
+	const tchar* it     = buffer.c_str();
+	const tchar* end    = it + buffer.length();
+	tchar*       endPtr = nullptr;
+
+	// Skip leading whitespace.
+	it = skipWhitespace(it, end);
+
+	if (it == end)
+		throw ParseException(Core::Fmt(TXT("Invalid number: '%s'"), buffer.c_str()));
+
+	errno = 0;
+
+	// Parse value.
+	T value = Traits::parse(it, &endPtr, 10);
+
+	if ( (endPtr == nullptr) || (errno != 0) )
+		throw ParseException(Core::Fmt(TXT("Invalid number: '%s'"), buffer.c_str()));
+
+	it = endPtr;
+
+	// Skip trailing whitespace.
+	it = skipWhitespace(it, end);
+
+	if ( (it != end) && (*it != TXT('\0')) )
+		throw ParseException(Core::Fmt(TXT("Invalid number: '%s'"), buffer.c_str()));
+
+	return value;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -119,41 +249,12 @@ tstring format(const uint64& /*value*/)
 template<>
 bool parse(const tstring& buffer)
 {
-	// Get buffer iterators.
-	const tchar* it  = buffer.c_str();
-	const tchar* end = it + buffer.length();
+	uint value = parseInteger< uint, FormatTraits<uint> >(buffer);
 
-	// Skip leading whitespace.
-	it = skipWhitespace(it, end);
-
-	if (it == end)
+	if ( (value != 0) && (value != 1) )
 		throw ParseException(Core::Fmt(TXT("Invalid boolean: '%s'"), buffer.c_str()));
 
-	bool value;
-
-	// Parse value.
-	if (*it == TXT('1'))
-	{
-		value = true;
-		++it;
-	}
-	else if (*it == TXT('0'))
-	{
-		value = false;
-		++it;
-	}
-	else
-	{
-		throw ParseException(Core::Fmt(TXT("Invalid boolean: '%s'"), buffer.c_str()));
-	}
-
-	// Skip trailing whitespace.
-	it = skipWhitespace(it, end);
-
-	if (it != end)
-		throw ParseException(Core::Fmt(TXT("Invalid boolean: '%s'"), buffer.c_str()));
-
-	return value;
+	return (value == 1);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -162,34 +263,7 @@ bool parse(const tstring& buffer)
 template<>
 int parse(const tstring& buffer)
 {
-	// Get buffer iterators.
-	const tchar* it     = buffer.c_str();
-	const tchar* end    = it + buffer.length();
-	tchar*       endPtr = nullptr;
-
-	// Skip leading whitespace.
-	it = skipWhitespace(it, end);
-
-	if (it == end)
-		throw ParseException(Core::Fmt(TXT("Invalid number: '%s'"), buffer.c_str()));
-
-	errno = 0;
-
-	// Parse value.
-	uint value = tstrtol(it, &endPtr, 10);
-
-	if ( (endPtr == nullptr) || (errno != 0) )
-		throw ParseException(Core::Fmt(TXT("Invalid number: '%s'"), buffer.c_str()));
-
-	it = endPtr;
-
-	// Skip trailing whitespace.
-	it = skipWhitespace(it, end);
-
-	if ( (it != end) && (*it != TXT('\0')) )
-		throw ParseException(Core::Fmt(TXT("Invalid number: '%s'"), buffer.c_str()));
-
-	return value;
+	return parseInteger< int, FormatTraits<int> >(buffer);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -198,45 +272,25 @@ int parse(const tstring& buffer)
 template<>
 uint parse(const tstring& buffer)
 {
-	// Get buffer iterators.
-	const tchar* it     = buffer.c_str();
-	const tchar* end    = it + buffer.length();
-	tchar*       endPtr = nullptr;
-
-	// Skip leading whitespace.
-	it = skipWhitespace(it, end);
-
-	if (it == end)
-		throw ParseException(Core::Fmt(TXT("Invalid number: '%s'"), buffer.c_str()));
-
-	errno = 0;
-
-	// Parse value.
-	uint value = tstrtoul(it, &endPtr, 10);
-
-	if ( (endPtr == nullptr) || (errno != 0) )
-		throw ParseException(Core::Fmt(TXT("Invalid number: '%s'"), buffer.c_str()));
-
-	it = endPtr;
-
-	// Skip trailing whitespace.
-	it = skipWhitespace(it, end);
-
-	if ( (it != end) && (*it != TXT('\0')) )
-		throw ParseException(Core::Fmt(TXT("Invalid number: '%s'"), buffer.c_str()));
-
-	return value;
+	return parseInteger< uint, FormatTraits<uint> >(buffer);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-//! Parse a 64-bit unsigned integer value from a string.
+// Parse a signed long long integer value from a string.
 
 template<>
-uint64 parse(const tstring& /*buffer*/)
+longlong parse(const tstring& buffer)
 {
-	ASSERT_FALSE();
+	return parseInteger< longlong, FormatTraits<longlong> >(buffer);
+}
 
-	return 0;
+////////////////////////////////////////////////////////////////////////////////
+//! Parse a unsigned long long integer value from a string.
+
+template<>
+ulonglong parse(const tstring& buffer)
+{
+	return parseInteger< ulonglong, FormatTraits<ulonglong> >(buffer);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
